acbd37ee2cb6b4cdb6393126bbee889a976b5441
XH <huazai2019126@gmail.com>
2023-12-22 14:09:42 +0800

    fix EntityComponentCore error

---
+++ Src/Runtime/Entity/IEntityComponent.cs
@@ -0,0 +1,11 @@
+/*
+ * @Author: xiang huan
+ * @Date: 2023-12-22 11:37:38
+ * @Description: 实体组件接口
+ * @FilePath: /lumiterra-scene-server/Assets/Plugins/SharedCore/Src/Runtime/Entity/IEntityComponent.cs
+ * 
+ */
+public interface IEntityComponent
+{
+    void InitEntity(EntityBase entity);
+}
