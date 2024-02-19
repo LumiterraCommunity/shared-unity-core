c07e6cd225d8664b69bcf242547a3fef787225ef
mangit6 <mangit0818@gmail.com>
2024-02-19 16:27:28 +0800

    pet data core

---
+++ Src/Runtime/Module/Entity/Data/PetDataCore.cs
@@ -0,0 +1,117 @@
+using UnityGameFramework.Runtime;
+
+
+/// <summary>
+/// 宠物专属数据
+/// </summary>
+public class PetDataCore : EntityBaseComponent
+{
+    /// <summary>
+    /// 宠物所有特性标记位
+    /// </summary>
+    public ePetAbility AllAbility { get; protected set; } = ePetAbility.None;
+    /// <summary>
+    /// 宠物主人ID
+    /// </summary>
+    public long OwnerId { get; protected set; }
+    /// <summary>
+    /// 当前跟随的目标
+    /// </summary>
+    public EntityBase FollowingTarget { get; protected set; }
+    /// <summary>
+    /// 当前是否正在跟随
+    /// </summary>
+    public bool IsFollowing => FollowingTarget != null;
+    /// <summary>
+    /// 宠物配置
+    /// </summary>
+    public DRPet PetCfg { protected set; get; }
+    public int PetCfgId => PetCfg == null ? -1 : PetCfg.Id;
+
+    public void SetOwnerId(long ownerId)
+    {
+        OwnerId = ownerId;
+    }
+
+    public void SetPetCfgId(int cfgID)
+    {
+        PetCfg = GFEntryCore.DataTable.GetDataTable<DRPet>().GetDataRow(cfgID);
+
+        if (PetCfg == null)
+        {
+            Log.Error($"Can not find pet cfg id:{cfgID}");
+        }
+    }
+
+    /// <summary>
+    /// 设置宠物所有特性
+    /// </summary>
+    /// <param name="ability"></param>
+    public void SetPetAbility(ePetAbility ability)
+    {
+        AllAbility = ability;
+    }
+
+    /// <summary>
+    /// 添加宠物特性
+    /// </summary>
+    /// <param name="ability"></param>
+    public void AddPetAbility(ePetAbility ability)
+    {
+        AllAbility |= ability;
+    }
+
+    /// <summary>
+    /// 移除宠物特性
+    /// </summary>
+    /// <param name="ability"></param>
+    public void RemovePetAbility(ePetAbility ability)
+    {
+        AllAbility &= ~ability;
+    }
+
+    /// <summary>
+    /// 是否有某个宠物特性
+    /// </summary>
+    /// <param name="ability"></param>
+    /// <returns></returns>
+    public bool HasPetAbility(ePetAbility ability)
+    {
+        return (AllAbility & ability) != 0;
+    }
+
+    /// <summary>
+    /// 获取跟随技能ID列表,一定会返回一个数组
+    /// </summary>
+    /// <returns></returns>
+    public int[] GetFollowingSkills()
+    {
+        if (!HasPetAbility(ePetAbility.SkillExtend) || !IsFollowing)
+        {
+            //没有扩展技能特性或者当前没在跟随
+            return new int[] { };
+        }
+
+        if (PetCfg == null)
+        {
+            Log.Error("PetCfg is null");
+            return new int[] { };
+        }
+
+        return PetCfg.ExtendSkill;
+    }
+
+    /// <summary>
+    /// 设置跟随目标
+    /// </summary>
+    /// <param name="target"></param>
+    public void SetFollowingTarget(EntityBase target)
+    {
+        if (FollowingTarget == target)
+        {
+            return;
+        }
+
+        FollowingTarget = target;
+    }
+}
