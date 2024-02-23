bd6622d76f741dddb05bce86a32f4bb27960f84e
RainZhuo <bigzhuozy@gmail.com>
2024-02-23 10:47:15 +0800

    Add New World Scene Config

--- Src/Runtime/Define/SceneAreaDefine.cs
+++ Src/Runtime/Define/SceneAreaDefine.cs
@@ -14,6 +14,7 @@ public enum eSceneArea : int
 {
     None,
     World = 10001,  //大世界
+    World2 = 10003,  //大世界
     Home = 10002,   //家园
     DungeonSceneLv1_3 = 20000,  //4-6级副本
     DungeonSceneLv4_6 = 20001,  //4-6级副本
