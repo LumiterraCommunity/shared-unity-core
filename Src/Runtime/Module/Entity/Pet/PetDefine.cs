d2e6859e2c845a077cca018c9f40b4030db058a1
mangit6 <mangit0818@gmail.com>
2024-02-26 21:25:48 +0800

    use bit to save pet ability

--- Src/Runtime/Module/Entity/Pet/PetDefine.cs
+++ Src/Runtime/Module/Entity/Pet/PetDefine.cs
@@ -1,13 +1,16 @@
+using GameMessageCore;
+
+
 /// <summary>
 /// 宠物能力
 /// </summary>
 public enum ePetAbility : int
 {
     None = 0,  //无
-    Battle = 1 << 0,  //战斗
-    Gather = 1 << 1,  //采集
-    Farming = 1 << 2,  //农场
-    Rescue = 1 << 3,  //救援
-    Riding = 1 << 4,  //骑乘
-    SkillExtend = 1 << 5,  //技能扩展属性
+    Battle = 1 << PetAbilityType.Battle,  //战斗
+    Gather = 1 << PetAbilityType.Gather,  //采集
+    Farming = 1 << PetAbilityType.Farming,  //农场
+    Rescue = 1 << PetAbilityType.Rescue,  //救援
+    Riding = 1 << PetAbilityType.Riding,  //骑乘
+    SkillExtend = 1 << PetAbilityType.SkillExtend,  //技能扩展属性
 }
