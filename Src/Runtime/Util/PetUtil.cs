65e2f32b6b4b690715b55479414d3f9b37e9314b
mangit6 <mangit0818@gmail.com>
2024-02-26 21:25:48 +0800

    use bit to save pet ability

---
+++ Src/Runtime/Util/PetUtil.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using GameMessageCore;
+
+
+/// <summary>
+/// 宠物相关工具
+/// </summary>
+public static class PetUtil
+{
+    /// <summary>
+    /// 宠物位移数组转换为宠物能力
+    /// </summary>
+    /// <param name="abilityBitOffsets"></param>
+    /// <returns></returns>
+    public static ePetAbility AbilityBitOffsets2ePetAbility(IEnumerable<PetAbilityType> abilityBitOffsets)
+    {
+        ePetAbility result = ePetAbility.None;
+        foreach (PetAbilityType ability in abilityBitOffsets)
+        {
+            result |= (ePetAbility)(1 << (int)ability);
+        }
+
+        return result;
+    }
+}
