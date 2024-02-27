7ff4189bfacb3c7d74510b8adaa167b412abba45
LuQian <LuQian11146@gmail.com>
2024-02-27 19:01:03 +0800

    animal data modify

--- Src/Runtime/Module/Home/Animal/AnimalUtilCore.cs
+++ Src/Runtime/Module/Home/Animal/AnimalUtilCore.cs
@@ -1,4 +1,3 @@
-using UnityGameFramework.Runtime;
 /// <summary>
 /// 畜牧工具
 /// </summary>
@@ -12,30 +11,5 @@ public class AnimalUtilCore
     public static int CalculateAnimalFavorabilityHeartNum(int favorabilityValue)
     {
         return favorabilityValue / TableUtil.GetGameValue(eGameValueID.animalFavorabilityValueEveryHeart).Value;
-    }
-
-    /// <summary>
-    /// 从monster实体上生成 AnimalBaseData数据 异常返回null
-    /// </summary>
-    /// <returns></returns>
-    public static AnimalBaseData GenerateAnimalBaseDataFromMonster(EntityBase monster)
-    {
-        if (monster == null)
-        {
-            Log.Error("GenerateAnimalBaseDataFromMonster monster is null");
-            return null;
-        }
-
-        DRMonster dRMonster = monster.GetComponent<MonsterDataCore>().DRMonster;
-        long createMs = TimeUtil.GetServerTimeStamp();
-        return new()
-        {
-            AnimalId = (ulong)monster.BaseData.Id,
-            Name = dRMonster.Name,
-            Cid = dRMonster.Id,
-            Favorability = 0,
-            CreateMs = createMs,
-            UpdateMs = createMs,
-        };
     }
 }
