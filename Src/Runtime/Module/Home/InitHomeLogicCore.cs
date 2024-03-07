d1235843457e5362dc54e7ae37c7e7a2a174b4e8
LuQian <LuQian11146@gmail.com>
2024-03-07 14:25:29 +0800

    pet AI order farming hoeing

--- Src/Runtime/Module/Home/InitHomeLogicCore.cs
+++ Src/Runtime/Module/Home/InitHomeLogicCore.cs
@@ -23,6 +23,7 @@ public class InitHomeLogicCore : MonoBehaviour
     private void GenerateSoil()
     {
         List<HomeResourcesArea> list = GFEntryCore.HomeResourcesAreaMgr.GetAreaListByType(HomeDefine.eHomeResourcesAreaType.farmland);
+        list.Sort((a, b) => a.Id.CompareTo(b.Id));//按照id排序生成土地
         for (int i = 0; i < list.Count; i++)
         {
             CreateSoil(list[i]);
@@ -31,22 +32,36 @@ public class InitHomeLogicCore : MonoBehaviour
 
     private void CreateSoil(HomeResourcesArea area)
     {
-
         Vector3 minPos = area.AreaBounds.min;
         Vector3 maxPos = area.AreaBounds.max;
         int countX = (int)System.MathF.Floor((maxPos.x - minPos.x) / HomeDefine.SOIL_SIZE.x);
         int countZ = (int)System.MathF.Floor((maxPos.z - minPos.z) / HomeDefine.SOIL_SIZE.z);
 
+        //不是用行扫描方式生成土地 而是回路方式生成土地类似贪吃蛇顺序 目的为了遍历时的土地在空间上是连续的 给宠物AI更自然
         for (int i = 0; i < countX; i++)
         {
-            for (int j = 0; j < countZ; j++)
+            if ((i & 1) == 0)//偶数行
             {
-                Vector3 pos = new(minPos.x + i * HomeDefine.SOIL_SIZE.x, minPos.y, minPos.z + j * HomeDefine.SOIL_SIZE.z);
-                ulong id = MathUtilCore.AreaToSoil(area.Id, i, j);
-                HomeModuleCore.SoilMgr.AddSoil(id, pos);
+                for (int j = 0; j < countZ; j++)
+                {
+                    AddSoil(minPos, area.Id, i, j);
+                }
+            }
+            else //奇数行
+            {
+                for (int j = countZ - 1; j >= 0; j--)
+                {
+                    AddSoil(minPos, area.Id, i, j);
+                }
             }
         }
 
+        static void AddSoil(Vector3 minPos, int areaId, int i, int j)
+        {
+            Vector3 pos = new(minPos.x + i * HomeDefine.SOIL_SIZE.x, minPos.y, minPos.z + j * HomeDefine.SOIL_SIZE.z);
+            ulong id = MathUtilCore.AreaToSoil(areaId, i, j);
+            HomeModuleCore.SoilMgr.AddSoil(id, pos);
+        }
     }
 
     /// <summary>
