beb17b3334447eb7e5b77d5b69701a5cf5b02687
LuQian <LuQian11146@gmail.com>
2024-03-10 14:46:29 +0800

    totem entity framework

--- Src/Runtime/Module/Home/SoilStatus/SoilHarvestStatusCore.cs
+++ Src/Runtime/Module/Home/SoilStatus/SoilHarvestStatusCore.cs
@@ -1,3 +1,6 @@
+using GameFramework.Fsm;
+using GameMessageCore;
+using UnityGameFramework.Runtime;
 using static HomeDefine;
 
 /// <summary>
@@ -7,12 +10,54 @@ public class SoilHarvestStatusCore : SoilStatusCore
 {
     public override eSoilStatus StatusFlag => eSoilStatus.Harvest;
 
-    public override eAction SupportAction => eAction.Harvest;
+    private eAction _supportAction = eAction.Harvest;
+    public override eAction SupportAction => _supportAction;
 
     protected override float AutoEnterNextStatusTime => 0;
 
+
+    private bool _waitGoSpecialFunctionStatus;
+
+    protected override void OnEnter(IFsm<SoilStatusCtrl> fsm)
+    {
+        base.OnEnter(fsm);
+
+        _waitGoSpecialFunctionStatus = false;
+
+        if (((SeedFunctionType)SoilData.DRSeed.FunctionType) != SeedFunctionType.None)
+        {
+            _supportAction = eAction.None;
+            _waitGoSpecialFunctionStatus = true;
+        }
+    }
+
+    protected override void OnLeave(IFsm<SoilStatusCtrl> fsm, bool isShutdown)
+    {
+        _supportAction = eAction.Harvest;
+        _waitGoSpecialFunctionStatus = false;
+
+        base.OnLeave(fsm, isShutdown);
+    }
+
+    protected override void OnUpdate(IFsm<SoilStatusCtrl> fsm, float elapseSeconds, float realElapseSeconds)
+    {
+        base.OnUpdate(fsm, elapseSeconds, realElapseSeconds);
+
+        if (_waitGoSpecialFunctionStatus)
+        {
+            _waitGoSpecialFunctionStatus = false;
+            ChangeState(eSoilStatus.SpecialFunction);
+        }
+    }
+
     protected override void OnExecuteHomeAction(eAction action, object actionData)
     {
+        if (_waitGoSpecialFunctionStatus)//防止同一帧收到消息的极端情况被收获掉了
+        {
+            Log.Error("当前状态等待进入特殊功能状态，不应该触发收获动作");
+            return;
+        }
+
         base.OnExecuteHomeAction(action, actionData);
 
         SoilData.ClearSeedData();
