ca7c654401cf00ccb6ffc485911858c892a4827c
XQ <807929448@qq.com>
2024-03-14 19:13:19 +0800

    load totem avatar and add harvest status

--- Src/Runtime/Module/Home/SoilStatus/SoilHarvestStatusCore.cs
+++ Src/Runtime/Module/Home/SoilStatus/SoilHarvestStatusCore.cs
@@ -1,16 +1,15 @@
 using static HomeDefine;
 
 /// <summary>
-/// 土地腐败成熟状态
+/// 土地等待收割状态 只有普通种子才能收割
 /// </summary>
-public class SoilRipePerishStatusCore : SoilStatusCore
+public class SoilHarvestStatusCore : SoilStatusCore
 {
-    public override eSoilStatus StatusFlag => eSoilStatus.RipePerish;
+    public override eSoilStatus StatusFlag => eSoilStatus.Harvest;
 
     public override eAction SupportAction => eAction.Harvest;
 
     protected override float AutoEnterNextStatusTime => 0;
-
     protected override void OnExecuteHomeAction(eAction action, object actionData)
     {
         base.OnExecuteHomeAction(action, actionData);
