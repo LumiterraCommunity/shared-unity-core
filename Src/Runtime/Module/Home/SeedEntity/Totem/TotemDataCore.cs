da41f05efafea586ce44649465ffe144ba84509d
LuQian <LuQian11146@gmail.com>
2024-03-15 23:15:31 +0800

    totem entity join interface

--- Src/Runtime/Module/Home/SeedEntity/Totem/TotemDataCore.cs
+++ Src/Runtime/Module/Home/SeedEntity/Totem/TotemDataCore.cs
@@ -1,8 +1,39 @@
+using GameMessageCore;
+
 /// <summary>
 /// 图腾上的专有数据
 /// </summary>
 public class TotemDataCore : SeedEntityComponentCore<SeedEntityCore>
 {
-    public float CurInvest;
-    public float CurPrize;
+    /// <summary>
+    /// 没有初始化的实体是无效的
+    /// </summary>
+    /// <value></value>
+    public bool Inited { get; private set; }
+
+    /// <summary>
+    /// 投资的图腾副本 未投资=0
+    /// </summary>
+    public int InvestDungeon;
+    /// <summary>
+    /// 投资份额
+    /// </summary>
+    public string PrizeLp;
+    /// <summary>
+    /// 回报份额
+    /// </summary>
+    public string RewardLp;
+
+    /// <summary>
+    /// 从网络数据初始化 只有初始化后才是有效实体
+    /// </summary>
+    /// <param name="proxyData"></param>
+    internal void InitFromProxyData(ProxyTotemData proxyData)
+    {
+        Inited = true;
+
+        InvestDungeon = proxyData.InvestDungeon;
+        PrizeLp = proxyData.PrizeLp;
+        RewardLp = proxyData.RewardLp;
+    }
 }
