3e3cd13e310cc0f84c6ff6075df664638d392ece
XQ <807929448@qq.com>
2024-03-19 19:49:56 +0800

    modify bigint default value

--- Src/Runtime/Util/MathUtilCore.cs
+++ Src/Runtime/Util/MathUtilCore.cs
@@ -176,14 +176,14 @@ public static class MathUtilCore
         float y = 4 * ((-height * t * t) + (height * t)) + Mathf.Lerp(start.y, end.y, t);
         return new Vector3(mid.x, y, mid.z);
     }
-    public static float BigInteger2Float(System.Numerics.BigInteger bigInteger, int exponent, int decimals)
+    public static float BigInteger2Float(System.Numerics.BigInteger bigInteger, int exponent, int decimals = 4)
     {
         double num = (double)(bigInteger / System.Numerics.BigInteger.Pow(10, exponent - decimals));
         float numValue = (float)(num / Math.Pow(10, decimals));
         return numValue;
     }
 
-    public static float BigInteger2Float(string bigIntegerStr, int exponent, int decimals)
+    public static float BigInteger2Float(string bigIntegerStr, int exponent, int decimals = 4)
     {
         _ = System.Numerics.BigInteger.TryParse(bigIntegerStr, out System.Numerics.BigInteger bigInteger);
         double num = (double)(bigInteger / System.Numerics.BigInteger.Pow(10, exponent - decimals));
