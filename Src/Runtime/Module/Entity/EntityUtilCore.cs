375216bcd194deb53d50ca40c8f2c7a9e89d0140
LuQian <LuQian11146@gmail.com>
2024-04-06 14:30:28 +0800

    player or pet death drop from damage weight random
    (cherry picked from commit 0f9d73e61a05adadc6a4c7ed45df5f0e5fcea71e)

--- Src/Runtime/Module/Entity/EntityUtilCore.cs
+++ Src/Runtime/Module/Entity/EntityUtilCore.cs
@@ -1,4 +1,5 @@
 using GameMessageCore;
+using UnityGameFramework.Runtime;
 
 
 /// <summary>
@@ -7,13 +8,47 @@ using GameMessageCore;
 public static class EntityUtilCore
 {
     /// <summary>
-    /// 判断是玩家类型 主要是由于客户端主角会用到MainPlayer类型
+    /// 判断实体类型是玩家单位
     /// </summary>
     /// <param name="entityType"></param>
     /// <returns></returns>
-    public static bool EntityTypeIsPlayer(EntityType entityType)
+    public static bool EntityTypeIsPlayerUnit(EntityType entityType)
     {
-        return entityType == EntityType.Player || entityType == EntityType.MainPlayer;
+        return entityType is EntityType.Player or EntityType.MainPlayer or EntityType.Pet or EntityType.HomeAnimal;
+    }
+
+    /// <summary>
+    /// 从实体单位上获取玩家id 不是玩家单位或者异常返回0 宠物返回主人 家园守卫返回家园主人 玩家返回自己
+    /// </summary>
+    /// <param name="entityBase"></param>
+    /// <returns></returns>
+    public static long GetPlayerIdFromEntityUnit(EntityBase entityBase)
+    {
+        if (entityBase == null)
+        {
+            return 0;
+        }
+
+        EntityType entityType = entityBase.BaseData.Type;
+        if (!EntityTypeIsPlayerUnit(entityType))
+        {
+            return 0;
+        }
+
+        if (entityType is EntityType.Player or EntityType.MainPlayer)
+        {
+            return entityBase.BaseData.Id;
+        }
+
+        if ((entityType is EntityType.Pet or EntityType.HomeAnimal) && entityBase.TryGetComponent(out PetDataCore petDataCore))
+        {
+            return petDataCore.OwnerId;
+        }
+
+        //家园守卫待补充
+
+        Log.Error($"GetPlayerIdFromEntityUnit,entityType:{entityType} id:{entityBase.BaseData.Id}");
+        return 0;
     }
 
     /// <summary>
