54a9c1841dd309bcc5fd5438d74e00f063530323
mangit6 <mangit0818@gmail.com>
2024-05-22 10:40:14 +0800

    update csv

---
+++ Src/Runtime/Csv/TableRow/DRInstructions.cs
@@ -0,0 +1,72 @@
+﻿//------------------------------------------------------------
+// 此文件由工具自动生成
+//------------------------------------------------------------
+
+using GameFramework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+
+/// <summary>
+/** __DATA_TABLE_COMMENT__*/
+/// </summary>
+public class DRInstructions : DataRowBase
+{
+    private int _id = 0;
+
+    /// <summary>
+    /// /**获取id-int。*/
+    /// </summary>
+    public override int Id => _id;
+
+    /// <summary>
+  /**获取name-string。*/
+    /// </summary>
+    public string Name
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取pageIds-int[]。*/
+    /// </summary>
+    public int[] PageIds
+    {
+        get;
+        private set;
+    }
+
+    public override bool ParseDataRow(string dataRowString, object userData)
+    {
+        string[] columnStrings = CSVSerializer.ParseCSVCol(dataRowString);
+
+        int index = 0;
+        _id = int.Parse(columnStrings[index++]);
+        Name = DataTableParseUtil.ParseString(columnStrings[index++]);
+        PageIds = DataTableParseUtil.ParseArray<int>(columnStrings[index++]);
+
+        return true;
+    }
+
+
+    public override bool ParseDataRow(byte[] dataRowBytes, int startIndex, int length, object userData)
+    {
+        using (MemoryStream memoryStream = new(dataRowBytes, startIndex, length, false))
+        {
+            using (BinaryReader binaryReader = new(memoryStream, Encoding.UTF8))
+            {
+                _id = binaryReader.Read7BitEncodedInt32();
+                Name = binaryReader.ReadString();
+                PageIds = binaryReader.ReadArray<Int32>();
+            }
+        }
+
+        return true;
+    }
+}
+
