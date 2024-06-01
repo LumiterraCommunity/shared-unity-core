54eee2c17bdddd54353ed13d5b93b4f2041ca1f6
LuQian <LuQian11146@gmail.com>
2024-06-01 14:54:24 +0800

    check home module function to core

--- Src/Runtime/Module/Home/HomeUtilCore.cs
+++ Src/Runtime/Module/Home/HomeUtilCore.cs
@@ -1,11 +1,42 @@
-using System;
 using System.Collections.Generic;
 using GameMessageCore;
+using UnityGameFramework.Runtime;
+using static HomeDefine;
 /// <summary>
 /// 家园工具core
 /// </summary>
 public static class HomeUtilCore
 {
+    /// <summary>
+    /// 检查某个场景配置是否是家园模块 out家园类型
+    /// </summary>
+    /// <param name="dRSceneArea"></param>
+    /// <param name="homeType"></param>
+    /// <returns></returns>
+    public static bool CheckIsHomeModuleScene(DRSceneArea dRSceneArea, out eHomeType homeType)
+    {
+        if (dRSceneArea == null)
+        {
+            Log.Error("HomeUtilCore CheckIsHomeModuleScene dRSceneArea is null.");
+            homeType = eHomeType.Unknown;
+            return false;
+        }
+
+        if (dRSceneArea.SceneType == (int)eSceneType.Home)
+        {
+            homeType = eHomeType.Personal;
+            return true;
+        }
+        else if (dRSceneArea.SceneType == (int)eSceneType.Instancing && dRSceneArea.SceneSubtype == (int)DungeonSubType.Home)
+        {
+            homeType = eHomeType.Instancing;
+            return true;
+        }
+
+        homeType = eHomeType.Unknown;
+        return false;
+    }
+
     /// <summary>
     /// 计算土地的所有已使用肥沃值
     /// </summary>
