48d7f5e905768c67797c7ea0368bb9c92444f6d5
XH <huazai2019126@gmail.com>
2024-06-08 14:43:37 +0800

    table change

--- Src/Runtime/Csv/TableDefine.cs
+++ Src/Runtime/Csv/TableDefine.cs
@@ -93,8 +93,7 @@ public enum eGameValueID
     PetRecallCD = 56, // 宠物召回CD,单位秒
     EnergyCraftBuilding = 59, //精力合成建筑id
     AllEnergyItemIds = 80, //所有精力道具列表
-
-    RefreshTaskNumLimit = 61, // 刷新任务次数限制
+    RefreshTaskNumLimit = 57, // 刷新任务次数限制
 }
 
 // public static class GameValueID
