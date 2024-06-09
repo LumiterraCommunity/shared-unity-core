6fd40e0dcb8eede2f7dbfe70ea7844afed2d42ca
XH <huazai2019126@gmail.com>
2024-06-09 16:18:47 +0800

    change STCCheckLevelTimeCore

--- Src/Runtime/Module/STEvent/Condition/STCCheckLevelTimeCore.cs
+++ Src/Runtime/Module/STEvent/Condition/STCCheckLevelTimeCore.cs
@@ -9,11 +9,14 @@
 using UnityGameFramework.Runtime;
 /// <summary>
 /// 检测关卡分时间
-/// DRSceneEventCondition.Parameters_1: [0]比较操作 [1]比较值
+/// DRSceneEventCondition.Parameters_1: [0]检测类型 [1]比较操作 [2]比较值
 /// </summary>//  
 public class STCCheckLevelTimeCore : STCCheckProgressCore
 {
+    protected eSTCCheckLevelScoreType CheckType = eSTCCheckLevelScoreType.Normal;
     protected IInstancingMgr InstancingMgr;
+
+    protected SceneAreaMgrCore SceneAreaMgrCore;
     public override void Init(DRSceneEventCondition cfg, SceneTriggerEvent sceneEvent)
     {
         base.Init(cfg, sceneEvent);
@@ -22,13 +25,25 @@ public class STCCheckLevelTimeCore : STCCheckProgressCore
             Log.Error("STCCheckLevelTimeCore Check Error: Parameters_1.Length < 2");
             return;
         }
-        Operation = (eSTConditionComparison)DRSceneEventCondition.Parameters_1[0];
-        TargetValue = DRSceneEventCondition.Parameters_1[1];
+
+        CheckType = (eSTCCheckLevelScoreType)DRSceneEventCondition.Parameters_1[0];
+        Operation = (eSTConditionComparison)DRSceneEventCondition.Parameters_1[1];
+        TargetValue = DRSceneEventCondition.Parameters_1[2] * MathUtilCore.PM;
         InstancingMgr = GFEntryCore.GetModule<IInstancingMgr>();
+        SceneAreaMgrCore = GFEntryCore.GetModule<SceneAreaMgrCore>();
     }
     public override bool Check()
     {
-        CurValue = (int)((TimeUtil.GetServerTimeStamp() - InstancingMgr.CurLevelStartTime) * TimeUtil.MS2S);
+        int levelTime = (int)((TimeUtil.GetServerTimeStamp() - InstancingMgr.CurLevelStartTime) * TimeUtil.MS2S);
+        float timeLimit = TableUtil.GetInstancingLevelTimeLimit(SceneAreaMgrCore.DefaultDRSceneArea, InstancingMgr.CurLevelIndex);
+        if (CheckType == eSTCCheckLevelScoreType.Normal)
+        {
+            CurValue = (int)((TimeUtil.GetServerTimeStamp() - InstancingMgr.CurLevelStartTime) * TimeUtil.MS2S);
+        }
+        else if (CheckType == eSTCCheckLevelScoreType.Percent)
+        {
+            CurValue = levelTime / timeLimit;
+        }
         return CheckValue(Operation, CurValue, TargetValue);
     }
 
