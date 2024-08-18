8d13a8e344cc91a0b2fb5803c59b445112538413
LuQian <LuQian11146@gmail.com>
2024-08-18 21:40:52 +0800

    pr comment

--- Src/Runtime/Module/Home/HomeUtilCore.cs
+++ Src/Runtime/Module/Home/HomeUtilCore.cs
@@ -186,7 +186,7 @@ public static class HomeUtilCore
         float fromLevel = entityAttribute.RefEntity.GetComponent<EntityAvatarDataCore>().GetAbilityLevel(eTalentType.gather);
         float toLevel = homeResource.GetActionLevel(targetCurAction);
         float probability = playerProficiency / resourceProficiency * Mathf.Pow(2, fromLevel - toLevel + 1);//概率 = 采集熟练度/需求熟练度 * 2^(采集装等 - 采集物等级 + 1)
-        return probability;
+        return Mathf.Clamp01(probability);
     }
 
     /// <summary>
