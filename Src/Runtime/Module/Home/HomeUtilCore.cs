b038e0f9c0798f82712bf99209d133363d62c966
LuQian <LuQian11146@gmail.com>
2024-08-18 14:02:54 +0800

    fix sword use gather ability level

--- Src/Runtime/Module/Home/HomeUtilCore.cs
+++ Src/Runtime/Module/Home/HomeUtilCore.cs
@@ -183,7 +183,7 @@ public static class HomeUtilCore
                 break;
         }
 
-        float fromLevel = entityAttribute.RefEntity.GetComponent<EntityAvatarDataCore>().AbilityLevel;
+        float fromLevel = entityAttribute.RefEntity.GetComponent<EntityAvatarDataCore>().GetAbilityLevel(eTalentType.gather);
         float toLevel = homeResource.GetActionLevel(targetCurAction);
         float probability = playerProficiency / resourceProficiency * Mathf.Pow(2, fromLevel - toLevel + 1);//概率 = 采集熟练度/需求熟练度 * 2^(采集装等 - 采集物等级 + 1)
         return probability;
