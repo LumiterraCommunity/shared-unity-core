8f7835c40429822a8f3c885441beec5345d8cf84
WIN-22FD8I0804D\Administrator <bigzhuozy@gmail.com>
2024-09-10 20:36:40 +0800

    add equipment enhancement about config

---
+++ Src/Runtime/Csv/TableRow/DRTotem.cs
@@ -0,0 +1,83 @@
+﻿//------------------------------------------------------------
+// 此文件由工具自动生成
+//------------------------------------------------------------
+
+using GameFramework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+
+/// <summary>
+/** __DATA_TABLE_COMMENT__*/
+/// </summary>
+public class DRTotem : DataRowBase
+{
+    private int _id = 0;
+
+    /// <summary>
+    /// /**获取id-int。*/
+    /// </summary>
+    public override int Id => _id;
+
+    /// <summary>
+  /**获取enhanceSucPro-int[]。*/
+    /// </summary>
+    public int[] EnhanceSucPro
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取protectArea-int。*/
+    /// </summary>
+    public int ProtectArea
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取totemEntity-string。*/
+    /// </summary>
+    public string TotemEntity
+    {
+        get;
+        private set;
+    }
+
+    public override bool ParseDataRow(string dataRowString, object userData)
+    {
+        string[] columnStrings = CSVSerializer.ParseCSVCol(dataRowString);
+
+        int index = 0;
+        EnhanceSucPro = DataTableParseUtil.ParseArray<int>(columnStrings[index++]);
+        _id = int.Parse(columnStrings[index++]);
+        ProtectArea = DataTableParseUtil.ParseInt(columnStrings[index++]);
+        TotemEntity = DataTableParseUtil.ParseString(columnStrings[index++]);
+
+        return true;
+    }
+
+
+    public override bool ParseDataRow(byte[] dataRowBytes, int startIndex, int length, object userData)
+    {
+        using (MemoryStream memoryStream = new(dataRowBytes, startIndex, length, false))
+        {
+            using (BinaryReader binaryReader = new(memoryStream, Encoding.UTF8))
+            {
+                EnhanceSucPro = binaryReader.ReadArray<Int32>();
+                _id = binaryReader.Read7BitEncodedInt32();
+                ProtectArea = binaryReader.Read7BitEncodedInt32();
+                TotemEntity = binaryReader.ReadString();
+            }
+        }
+
+        return true;
+    }
+}
+
