c3e2e141636b2ff60182eeb154350b123ddf75b2
mangit6 <mangit0818@gmail.com>
2024-10-16 14:17:58 +0800

    add leader field,init from net data

--- Src/Runtime/Module/Entity/Data/WagonDataCore.cs
+++ Src/Runtime/Module/Entity/Data/WagonDataCore.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using GameMessageCore;
 
 
@@ -12,18 +11,25 @@ public class WagonDataCore : EntityBaseComponent
     public long AcceptTime { private set; get; }//接镖时间
     public long ExpireTime { private set; get; }//过期时间
     public long EscortTeamId { private set; get; }//护送队伍ID
+    public long LeaderId { private set; get; }//队长ID
     // public List<long> TeamMembers { private set; get; }//护送者ID
     public EscortWagonFailReason FailReason { get; private set; } = EscortWagonFailReason.Unknown;//Unknown表示不失败
     public bool Arrived { get; private set; }//是否到达目
 
-    public void Init(long teamId, int cid)
+    public void Init(long teamId, long leaderId, int cid)
     {
         EscortTeamId = teamId;
+        LeaderId = leaderId;
         Cid = cid;
         Cfg = GFEntryCore.DataTable.GetDataTable<DREscortWagon>().GetDataRow(cid);
 
         AcceptTime = TimeUtil.GetServerTimeStamp();
         ExpireTime = AcceptTime + Cfg.MaxExistenceTime * TimeUtil.S2MSInt;
+    }
+
+    public void Init(Wagon wagonNetData)
+    {
+        Init(wagonNetData.EscortTeamId, wagonNetData.LeaderId, wagonNetData.Cid);
     }
 
     public void SetFailReason(EscortWagonFailReason reason)
