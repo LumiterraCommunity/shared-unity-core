85bcb583886234b6a9405ab1452012ad12900c9e
WIN-22FD8I0804D\Administrator <bigzhuozy@gmail.com>
2024-10-18 16:02:58 +0800

    update all config

---
+++ Src/Runtime/Csv/TableRow/DREscortWagon.cs
@@ -0,0 +1,149 @@
+﻿//------------------------------------------------------------
+// 此文件由工具自动生成
+//------------------------------------------------------------
+
+using GameFramework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+
+/// <summary>
+/** __DATA_TABLE_COMMENT__*/
+/// </summary>
+public class DREscortWagon : DataRowBase
+{
+    private int _id = 0;
+
+    /// <summary>
+    /// /**获取id-int。*/
+    /// </summary>
+    public override int Id => _id;
+
+    /// <summary>
+  /**获取initialAttribute-int[][]。*/
+    /// </summary>
+    public int[][] InitialAttribute
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取lv-int。*/
+    /// </summary>
+    public int Lv
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取lvLimit-int。*/
+    /// </summary>
+    public int LvLimit
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取maxAccept-int。*/
+    /// </summary>
+    public int MaxAccept
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取maxExistenceTime-int。*/
+    /// </summary>
+    public int MaxExistenceTime
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取name-string。*/
+    /// </summary>
+    public string Name
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取rewardId-int。*/
+    /// </summary>
+    public int RewardId
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取roleAssetId-int。*/
+    /// </summary>
+    public int RoleAssetId
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取tickets-int[][]。*/
+    /// </summary>
+    public int[][] Tickets
+    {
+        get;
+        private set;
+    }
+
+    public override bool ParseDataRow(string dataRowString, object userData)
+    {
+        string[] columnStrings = CSVSerializer.ParseCSVCol(dataRowString);
+
+        int index = 0;
+        _id = int.Parse(columnStrings[index++]);
+        InitialAttribute = DataTableParseUtil.ParseArrayList<int>(columnStrings[index++]);
+        Lv = DataTableParseUtil.ParseInt(columnStrings[index++]);
+        LvLimit = DataTableParseUtil.ParseInt(columnStrings[index++]);
+        MaxAccept = DataTableParseUtil.ParseInt(columnStrings[index++]);
+        MaxExistenceTime = DataTableParseUtil.ParseInt(columnStrings[index++]);
+        Name = DataTableParseUtil.ParseString(columnStrings[index++]);
+        RewardId = DataTableParseUtil.ParseInt(columnStrings[index++]);
+        RoleAssetId = DataTableParseUtil.ParseInt(columnStrings[index++]);
+        Tickets = DataTableParseUtil.ParseArrayList<int>(columnStrings[index++]);
+
+        return true;
+    }
+
+
+    public override bool ParseDataRow(byte[] dataRowBytes, int startIndex, int length, object userData)
+    {
+        using (MemoryStream memoryStream = new(dataRowBytes, startIndex, length, false))
+        {
+            using (BinaryReader binaryReader = new(memoryStream, Encoding.UTF8))
+            {
+                _id = binaryReader.Read7BitEncodedInt32();
+                InitialAttribute = binaryReader.ReadArrayList<Int32>();
+                Lv = binaryReader.Read7BitEncodedInt32();
+                LvLimit = binaryReader.Read7BitEncodedInt32();
+                MaxAccept = binaryReader.Read7BitEncodedInt32();
+                MaxExistenceTime = binaryReader.Read7BitEncodedInt32();
+                Name = binaryReader.ReadString();
+                RewardId = binaryReader.Read7BitEncodedInt32();
+                RoleAssetId = binaryReader.Read7BitEncodedInt32();
+                Tickets = binaryReader.ReadArrayList<Int32>();
+            }
+        }
+
+        return true;
+    }
+}
+
---
+++ Src/Runtime/Csv/TableRow/DRSceneAreaChapter.cs
@@ -0,0 +1,116 @@
+﻿//------------------------------------------------------------
+// 此文件由工具自动生成
+//------------------------------------------------------------
+
+using GameFramework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+
+/// <summary>
+/** __DATA_TABLE_COMMENT__*/
+/// </summary>
+public class DRSceneAreaChapter : DataRowBase
+{
+    private int _id = 0;
+
+    /// <summary>
+    /// /**获取id-int。*/
+    /// </summary>
+    public override int Id => _id;
+
+    /// <summary>
+  /**获取bossEntityList-int[]。*/
+    /// </summary>
+    public int[] BossEntityList
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取entityList-int[][]。*/
+    /// </summary>
+    public int[][] EntityList
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取entityMaximum-int。*/
+    /// </summary>
+    public int EntityMaximum
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取entityType-int。*/
+    /// </summary>
+    public int EntityType
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取name-string。*/
+    /// </summary>
+    public string Name
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+  /**获取sceneAreaId-int。*/
+    /// </summary>
+    public int SceneAreaId
+    {
+        get;
+        private set;
+    }
+
+    public override bool ParseDataRow(string dataRowString, object userData)
+    {
+        string[] columnStrings = CSVSerializer.ParseCSVCol(dataRowString);
+
+        int index = 0;
+        BossEntityList = DataTableParseUtil.ParseArray<int>(columnStrings[index++]);
+        EntityList = DataTableParseUtil.ParseArrayList<int>(columnStrings[index++]);
+        EntityMaximum = DataTableParseUtil.ParseInt(columnStrings[index++]);
+        EntityType = DataTableParseUtil.ParseInt(columnStrings[index++]);
+        _id = int.Parse(columnStrings[index++]);
+        Name = DataTableParseUtil.ParseString(columnStrings[index++]);
+        SceneAreaId = DataTableParseUtil.ParseInt(columnStrings[index++]);
+
+        return true;
+    }
+
+
+    public override bool ParseDataRow(byte[] dataRowBytes, int startIndex, int length, object userData)
+    {
+        using (MemoryStream memoryStream = new(dataRowBytes, startIndex, length, false))
+        {
+            using (BinaryReader binaryReader = new(memoryStream, Encoding.UTF8))
+            {
+                BossEntityList = binaryReader.ReadArray<Int32>();
+                EntityList = binaryReader.ReadArrayList<Int32>();
+                EntityMaximum = binaryReader.Read7BitEncodedInt32();
+                EntityType = binaryReader.Read7BitEncodedInt32();
+                _id = binaryReader.Read7BitEncodedInt32();
+                Name = binaryReader.ReadString();
+                SceneAreaId = binaryReader.Read7BitEncodedInt32();
+            }
+        }
+
+        return true;
+    }
+}
+
