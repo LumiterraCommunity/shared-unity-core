fe14e7632a5d56fbf4e0287cf87dc03403c435b3
WIN-22FD8I0804D\Administrator <bigzhuozy@gmail.com>
2024-11-14 18:10:55 +0800

    update config

--- Src/Runtime/Csv/TableRow/DRTalentTree.cs
+++ Src/Runtime/Csv/TableRow/DRTalentTree.cs
@@ -105,6 +105,15 @@ public class DRTalentTree : DataRowBase
     }
 
     /// <summary>
+  /**获取preNode-int[]。*/
+    /// </summary>
+    public int[] PreNode
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
   /**获取resetCost-int。*/
     /// </summary>
     public int ResetCost
@@ -141,15 +150,6 @@ public class DRTalentTree : DataRowBase
     }
 
     /// <summary>
-  /**获取preNode-int[]。*/
-    /// </summary>
-    public int[] PreNode
-    {
-        get;
-        private set;
-    }
-
-    /// <summary>
   /**获取upgradeToken-int[]。*/
     /// </summary>
     public int[] UpgradeToken
@@ -173,11 +173,11 @@ public class DRTalentTree : DataRowBase
         Layer = DataTableParseUtil.ParseInt(columnStrings[index++]);
         LvLimit = DataTableParseUtil.ParseInt(columnStrings[index++]);
         Name = DataTableParseUtil.ParseString(columnStrings[index++]);
+        PreNode = DataTableParseUtil.ParseArray<int>(columnStrings[index++]);
         ResetCost = DataTableParseUtil.ParseInt(columnStrings[index++]);
         Type = DataTableParseUtil.ParseInt(columnStrings[index++]);
         UpgradeEXP = DataTableParseUtil.ParseArray<int>(columnStrings[index++]);
         UpgradeRequireTreeLv = DataTableParseUtil.ParseInt(columnStrings[index++]);
-        PreNode = DataTableParseUtil.ParseArray<int>(columnStrings[index++]);
         UpgradeToken = DataTableParseUtil.ParseArray<int>(columnStrings[index++]);
 
         return true;
@@ -200,11 +200,11 @@ public class DRTalentTree : DataRowBase
                 Layer = binaryReader.Read7BitEncodedInt32();
                 LvLimit = binaryReader.Read7BitEncodedInt32();
                 Name = binaryReader.ReadString();
+                PreNode = binaryReader.ReadArray<Int32>();
                 ResetCost = binaryReader.Read7BitEncodedInt32();
                 Type = binaryReader.Read7BitEncodedInt32();
                 UpgradeEXP = binaryReader.ReadArray<Int32>();
                 UpgradeRequireTreeLv = binaryReader.Read7BitEncodedInt32();
-                PreNode = binaryReader.ReadArray<Int32>();
                 UpgradeToken = binaryReader.ReadArray<Int32>();
             }
         }
