15169438956bdbe49017fb06b05b59d6db094f34
XH <huazai2019126@gmail.com>
2024-11-20 15:52:19 +0800

    table change

--- Src/Runtime/Define/AvatarDefineCore.cs
+++ Src/Runtime/Define/AvatarDefineCore.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using GameMessageCore;
+using UnityEngine;
 /// <summary>
 /// Avatar related define
 /// </summary>
@@ -39,4 +40,41 @@ public static class AvatarDefineCore
     /// </summary>
     /// <returns></returns>
     public static readonly List<AvatarPosition> AllPartList = EquipmentPartList.Concat(AppearancePartList).ToList();
+
+    public const float EQUIPMENT_COLOR_ALPHA = 0.9f;
+    public class EquipmentEnhanceEffect
+    {
+        public int MinLevel;
+        public int MaxLevel;
+        public Color Color;    //颜色
+        public float Intensity; //亮度
+    }
+
+    /// <summary>
+    /// 装备强化效果配置
+    /// </summary>
+    public static readonly List<EquipmentEnhanceEffect> EquipmentEnhanceEffects = new()
+    {
+        new EquipmentEnhanceEffect
+        {
+            MinLevel = 0,
+            MaxLevel = 1,
+            Color = new Color(0.5f, 0.5f, 0.5f),
+            Intensity = 100f,
+        },
+        new EquipmentEnhanceEffect
+        {
+            MinLevel = 2,
+            MaxLevel = 10,
+            Color = new Color(0.5f, 0.5f, 0.5f),
+            Intensity = 100f,
+        },
+        new EquipmentEnhanceEffect
+        {
+            MinLevel = 11,
+            MaxLevel = 30,
+            Color = new Color(0.5f, 0.5f, 0.5f),
+            Intensity = 100f,
+        },
+    };
 }
